bd066b211bbb98935fe77ac15bd546a24c1fe1ca
Judith Pillado <judith.pillado@gmail.com>
2020-05-27 10:23:10 -0500

    Add test it exists

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,4 +5,12 @@ require './lib/patron'
 
 class PatronTest < Minitest::Test
 
-end 
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @patron_1
+  end 
+
+end
