f883680fc7c4da70b6e58a33c22983efb6b82e66
Judith Pillado <judith.pillado@gmail.com>
2020-05-27 10:37:38 -0500

    Add test it exists

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -6,4 +6,12 @@ require './lib/museum'
 
 class MuseumTest < Minitest::Test
 
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end 
+
 end
